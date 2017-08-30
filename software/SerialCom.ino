6b66f0d8b695969ed7e911c4337c6a7b51589b74
MJ-MJ <miromujing@gmail.com>
2017-08-30 19:26:49 +1000

    Add files via upload

---
+++ software/SerialCom.ino
@@ -0,0 +1,32 @@
+int blockNo;
+int r;
+int g;
+int b;
+String rr;
+String gg;
+String bb;
+String colour[3]={rr,gg,bb};
+
+
+void SerialCom() {
+  // convert int r,g,b into a string of 3 characters
+  int col[3]={r,g,b};
+  for (int i=0;i<3;i++){
+    if (col[i]>=0 && col[i]<=9){
+      colour[i]="00"+ (String)col[i];
+    }else if(col[i]>=10 && col[i]<=99){
+      colour[i]="0"+ (String)col[i];
+    }else if(col[i]>=100){
+      colour[i]= (String)col[i];
+    }
+  }
+  
+  // Serial communication
+  Serial.print('s');
+  Serial.print(blockNo);
+  Serial.print(colour[0]);
+  Serial.print(colour[1]);
+  Serial.print(colour[2]);
+  Serial.print('e');
+}
+
