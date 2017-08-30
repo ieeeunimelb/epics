5f2ce4c010b58aa6d1a0990e37dbbf4799ce8072
MJ-MJ <miromujing@gmail.com>
2017-08-30 18:42:42 +1000

    Add files via upload

---
+++ software/RainbowBox.ino
@@ -0,0 +1,124 @@
+// for only one RGB LED
+int redPin= 12;
+int greenPin = 11;
+int bluePin = 10;
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  int state=random(0, 100);
+  float x=state/100.0;
+  Serial.println(x);
+    if (x<0.1){
+      digitalWrite(redPin,LOW);
+      digitalWrite(greenPin,LOW);
+      digitalWrite(bluePin,LOW);
+      Serial.println("OFF");
+    } else {
+      int r=random(0, 255);
+      int g=random(0, 255);
+      int b=random(0, 255);
+      Serial.print(r);
+      Serial.print(", ");
+      Serial.print(g);
+      Serial.print(", ");
+      Serial.println(b);
+      analogWrite(redPin,r);
+      analogWrite(greenPin,g);
+      analogWrite(bluePin,b);
+    }
+  delay(500);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//// all the following pins should be PWM
+//
+//// LED pins of each block
+//int Block11_LED[3] = {1,2,3};
+//int Block12_LED[3] = {4,5,6};
+//int Block13_LED[3] = {7,8,9};
+//int Block21_LED[3] = {10,11,12};
+//int Block22_LED[3] = {13,14,15};
+//int Block23_LED[3] = {16,17,18};
+//int Block31_LED[3] = {19,20,21};
+//int Block32_LED[3] = {22,23,24};
+//int Block33_LED[3] = {25,26,27};
+//
+//int Block_LED[9][3]={Block11_LED[3],Block12_LED[3],Block13_LED[3],Block21_LED[3],
+//    Block22_LED[3],Block23_LED[3],Block31_LED[3],Block32_LED[3],Block33_LED[3]};
+//
+//void setup() {
+//  // all LED pins as OUTPUT
+//  for (int i=0;i<9;i++){
+//    for (int j=0;j<3;j++){
+//      pinMode(Block_LED[4][j],OUTPUT);
+//    }
+//  }
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//  funLight1();
+//  delay(500);
+//}
+//
+//
+//void funLight1(){
+//  for (int i=0;i<9;i++){
+//    int state=random(0, 100);
+//    float x=state/100.0;
+//    if (x<0.1){
+//      for (int j=0;j<3;j++){
+//        digitalWrite(Block_LED[i][j],LOW);
+//      }
+//    } else {
+//      int r=random(0, 255);
+//      int g=random(0, 255);
+//      int b=random(0, 255);
+//      analogWrite(Block_LED[i][0],r);
+//      analogWrite(Block_LED[i][1],g);
+//      analogWrite(Block_LED[i][2],b);
+//    }
+//  }
+//}
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//int redPin= 7;
+//int greenPin = 6;
+//int bluePin = 5;
+//void setup() {
+//  pinMode(redPin, OUTPUT);
+//  pinMode(greenPin, OUTPUT);
+//  pinMode(bluePin, OUTPUT);
+//}
+//void loop() {
+//  setColor(255, 0, 0); // Red Color
+//  delay(1000);
+//  setColor(0, 255, 0); // Green Color
+//  delay(1000);
+//  setColor(0, 0, 255); // Blue Color
+//  delay(1000);
+//  setColor(255, 255, 255); // White Color
+//  delay(1000);
+//  setColor(170, 0, 255); // Purple Color
+//  delay(1000);
+//}
+//void setColor(int redValue, int greenValue, int blueValue) {
+//  analogWrite(redPin, redValue);
+//  analogWrite(greenPin, greenValue);
+//  analogWrite(bluePin, blueValue);
+//}
