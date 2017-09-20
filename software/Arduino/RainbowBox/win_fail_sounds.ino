0ae168eea77ae33280dd136cfc9d2a95d26f3961
hwahidin <31471984+hwahidin@users.noreply.github.com>
2017-09-20 20:14:17 +1000

    Add files via upload

---
+++ software/Arduino/RainbowBox/win_fail_sounds.ino
@@ -0,0 +1,64 @@
+#include "musical_notes.h"
+
+int speakerPin = 9; // speaker connected to digital pin 9 
+      
+void setup()    
+{   
+      pinMode(speakerPin, OUTPUT); // sets the speakerPin to be an output 
+}    
+      
+void loop()
+{       
+    win();
+    fail();
+    exit(0);
+}     
+     
+void beep (int speakerPin, float noteFrequency, long noteDuration)
+{    
+  int x;
+  // Convert the frequency to microseconds
+  float microsecondsPerWave = 1000000/noteFrequency;
+  // Calculate how many HIGH/LOW cycles there are per millisecond
+  float millisecondsPerCycle = 1000/(microsecondsPerWave * 2);
+  // Multiply noteDuration * number or cycles per millisecond
+  float loopTime = noteDuration * millisecondsPerCycle;
+  // Play the note for the calculated loopTime.
+  for (x=0;x<loopTime;x++)   
+          {   
+              digitalWrite(speakerPin,HIGH); 
+              delayMicroseconds(microsecondsPerWave); 
+              digitalWrite(speakerPin,LOW); 
+              delayMicroseconds(microsecondsPerWave); 
+          } 
+}     
+
+void win(){
+          beep(speakerPin, note_A7,100); //A 
+          beep(speakerPin, note_G7,100); //G 
+          beep(speakerPin, note_E7,100); //E 
+          beep(speakerPin, note_C7,100); //C
+          beep(speakerPin, note_D7,100); //D 
+          beep(speakerPin, note_B7,100); //B 
+          beep(speakerPin, note_F7,100); //F 
+          beep(speakerPin, note_C8,100); //C 
+          beep(speakerPin, note_A7,100); //A 
+          beep(speakerPin, note_G7,100); //G 
+          beep(speakerPin, note_E7,100); //E 
+          beep(speakerPin, note_C7,100); //C
+          beep(speakerPin, note_D7,100); //D 
+          beep(speakerPin, note_B7,100); //B 
+          beep(speakerPin, note_F7,100); //F 
+          beep(speakerPin, note_C8,100); //C 
+}
+
+
+void fail() {
+
+    beep(speakerPin,note_B4,250);
+    beep(speakerPin,note_F4,250);
+    beep(speakerPin,note_C4,500);
+
+}
+
+
